52438d97544600e2f3576ef4a3e877cd96d4d832
Melvin Nunes <melvinnunes14@gmail.com>
2025-02-17 14:48:04 +0200

    feat: added basic data transfer interfaces

---
+++ src/interfaces/dtos/authDTO.ts
@@ -0,0 +1,12 @@
+export interface Login {
+  email: string;
+  password: string;
+}
+
+export interface Register {
+  email: string;
+  first_name: string;
+  last_name: string;
+  phone_number: string;
+  password: string;
+}
---
+++ src/interfaces/dtos/profileDTO.ts
@@ -0,0 +1,6 @@
+export interface ProfileCreationData {
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  userId: string;
+}
