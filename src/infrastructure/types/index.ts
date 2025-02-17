e74d1b72e7db556dce3b0eef50689e0e286c30ce
Melvin Nunes <melvinnunes14@gmail.com>
2025-02-17 14:47:13 +0200

    feat: added insfrastructure common types

---
+++ src/infrastructure/types/index.ts
@@ -0,0 +1,48 @@
+import { Request } from "express";
+import { TFunction } from "i18next";
+
+export type AuthUser = {
+  name: string;
+  iat: string;
+  exp: string;
+};
+
+export class ApplicationException extends Error {
+  statusCode: number;
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode || 500;
+    this.name = "ApplicationException";
+  }
+}
+
+export type File = {
+  fieldname: string;
+  originalname: string;
+  enconding: string;
+  mimetype: string;
+  destination: string;
+  filename: string;
+  path: string;
+  size: number;
+};
+
+export interface RequestWithUser extends Request {
+  user?: AuthUser;
+  t?: TFunction;
+}
+
+export interface RegisterRequest extends Request {
+  file: File;
+  t?: TFunction;
+}
+
+export interface RegisterWithUserAndFile extends Request {
+  file: File;
+  user?: AuthUser;
+  t?: TFunction;
+}
+
+export interface RequestWTranslation extends Request {
+  t?: TFunction;
+}
