c9ed17e1aaf54a98546943a75feef371925732a6
Melvin Nunes <melvinnunes14@gmail.com>
2025-02-17 14:43:50 +0200

    feat: added more routes and to the routes file

---
+++ src/application/routes/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import HealthController from "../../interfaces/controllers/healthController";
+import UserController from "../../interfaces/controllers/userController";
+
+import { authenticateToken } from "../middlewares/tokenAuthMiddleware";
+import AuthController from "../../interfaces/controllers/authController";
+import {
+  LoginRequest,
+  RegistrationRequest,
+} from "../../interfaces/request/authentication";
+import { validateData } from "../middlewares/requestValidationMiddleware";
+
+const router = express.Router();
+
+router.get("/health", HealthController.checkApiHealth);
+router.post("/login", validateData(LoginRequest), AuthController.login);
+router.post(
+  "/register",
+  validateData(RegistrationRequest),
+  AuthController.register
+);
+
+router.get("/me", authenticateToken, UserController.getOnlineUser);
+router.get("/users", authenticateToken, UserController.getAllUsers);
+
+module.exports = router;
